9483d22151fd5ae29a39638a2ee1c00af131d3ea
milivanili300 <113971449+milivanili300@users.noreply.github.com>
2023-01-10 22:09:59 +0100

    10 Commit 10.01.23 Neue Tour funktioniert, richtige Touren ausgeben noch in bearbeitung

--- Tourenplaner/datenbank.py
+++ Tourenplaner/datenbank.py
@@ -1,23 +1,13 @@
-
+import json
 def auslesen():
-    with open("dict_zugriff_besipiel.txt", "r") as open_file:
-        inhalt = open_file.read()
+    with open("data.json", "r") as open_file:
+        inhalt = json.load(open_file)
     return inhalt
 
 
-def touren_laden():
-    #test2
-    touren = auslesen()
-    touren_liste = touren.split("\n")
-    neue_liste = []
-    for eintrag in touren_liste:
-        aufgabe, deadline = eintrag.split(",")
-        neue_liste.append([aufgabe, deadline])
-    return neue_liste
 
-
-def abspeichern(aufgabe, deadline):
+def abspeichern( name, tour):
     current_content = auslesen()
-    new_content = current_content + f"\n{aufgabe},{deadline}"
-    with open("dict_zugriff_besipiel.txt", "w") as open_file:
-        open_file.write(new_content)
+    current_content[name] = tour
+    with open("data.json", "w", encoding="utf8") as open_file:
+        json.dump(current_content, open_file, indent=4)
--- Tourenplaner/main.py
+++ Tourenplaner/main.py
@@ -4,50 +4,71 @@ from flask import request
 import plotly.express as px
 from plotly.offline import plot
 
-from datenbank import abspeichern, auslesen, touren_laden
+from datenbank import abspeichern, auslesen
 
 app = Flask("Tourenplaner")
 
+
 @app.route("/", methods=["GET", "POST"])
 def start():
+    selection = {}
+
     if request.method == "POST":
+        touren = auslesen()
         dauer = request.form["dauer"]
-        return "1" if len(a) == 1 else "0"
-    return render_template('kategorisierung.html')
+        hoehenmeter = request.form["hoehenmeter"]
+        tiefenmeter = request.form["tiefenmeter"]
+        schwierigkeit = request.form["schwierigkeit"]
+        erreichbarkeit = request.form["erreichbarkeit"]
+        gefahrenstufe = request.form["gefahrenstufe"]
+
+        for key, value in touren.items():
+            if value["hoehenmeter"] == hoehenmeter:
+                if value["dauer"] == dauer:
+                    if value["tiefenmeter"] == tiefenmeter:
+                        if value["schwierigkeit"] == schwierigkeit:
+                            if value["gefahrenstufe"] == gefahrenstufe:
+                                if value["erreichbarkeit"] == erreichbarkeit:
+                                    selection[touren] = {
+                                        "dauer": dauer,
+                                        "tiefenmeter": tiefenmeter,
+                                        "hoehenmeter": hoehenmeter,
+                                        "schwierigkeit": schwierigkeit,
+                                        "gefahrenstufe": gefahrenstufe,
+                                        "erreichbarkeit": erreichbarkeit
+                                    }
+    return render_template('kategorisierung.html', selection=selection)
+
 
 @app.route("/add", methods=["GET", "POST"])
 def add_new_tour():
-    if request.method == "GET":
-        return render_template("neue_tour.html", seitentitel="Todo Eingabe")
 
     if request.method == "POST":
-        aufgabe = request.form['aufgabe']
-        deadline = request.form['deadline']
-        print(f"Request Form Aufgabe: {aufgabe}")
-        print(f"Request Form Deadline: {deadline}")
-        abspeichern(aufgabe, deadline)
-        return "hät funktioniert"
+        name = request.form["Name der Tour"]
+        gefahrenstufe = request.form['gefahrenstufe']
+        oev = request.form['erreichbarkeit']
+        hoehenmeter = request.form['hoehenmeter']
+        tiefenmeter = request.form['tiefenmeter']
+        dauer = request.form['dauer']
+        schwierigkeit = request.form['schwierigkeit']
+
+        tour = {
+            'gefahrenstufe': gefahrenstufe,
+            'erreichbarkeit': oev,
+            'hoehenmeter': hoehenmeter,
+            'tiefenmeter': tiefenmeter,
+            'dauer': dauer,
+            'schwierigkeit': schwierigkeit,
+        }
+        abspeichern(name, tour)
+
+    return render_template("neue_tour.html")
 
 
 @app.route("/overview")
 def grafik():
     return "nope"
-    """
-    todos = todos_laden()
-    deadlines = {}
-    for todo in todos:
-        if todo[1] not in deadlines:
-            deadlines[todo[1]] = 1
-        else:
-            deadlines[todo[1]] += 1
 
-    x = deadlines.keys()
-    y = deadlines.values()
-    fig = px.bar(x=x, y=y)
-    div = plot(fig, output_type="div")
-
-    return render_template("overview.html", barchart=div, seitentitel="Tourenübersicht")
-    """
 
 if __name__ == "__main__":
     app.run(debug=True, port=5550)
